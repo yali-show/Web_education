36f48d1d999159acb74de6cfe149a4f338752859
ilyabeliy <ya.ila.white@gmail.com>
2022-10-10 16:21:24 +0200

    add admin for feedbacks

--- Django_project/feedbacks/admin.py
+++ Django_project/feedbacks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from feedbacks.models import Feedback
 
-# Register your models here.
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['user', 'text', 'rating']
